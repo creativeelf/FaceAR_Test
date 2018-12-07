245d0de9d510f757a48942bc45833ca440775d76
Sagar Ramesh <sagarram@usc.edu>
2018-12-07 01:10:45 -0800

    Uploaded Brenda’s assets, scripts to detect collisions with food, and scripts to stream data from Kinect. Need to build UI to expose life, score, and game over state.

---
+++ Assets/Tummy/Scripts/TransformCube.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransformCube : MonoBehaviour {
+
+    private GameObject target;
+    public GameObject sceneManager;
+
+	// Use this for initialization
+	void Start () {
+        target = this.gameObject;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        target.transform.localPosition = sceneManager.GetComponent<ScoreClient>().GetPosition();
+        target.transform.localEulerAngles = sceneManager.GetComponent<ScoreClient>().GetRotation();
+	}
+}
